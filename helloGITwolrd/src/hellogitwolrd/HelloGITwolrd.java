621bba9c600b672ac1a6b9ba0c31fcf149b20384
91021441 <91021441@LRNWNWRK-014.learn.cpp.ac.nz>
2018-09-28 17:21:46 +1200

    Test

---
+++ helloGITwolrd/src/hellogitwolrd/HelloGITwolrd.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellogitwolrd;
+
+/**
+ *
+ * @author 91021441
+ */
+public class HelloGITwolrd {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
